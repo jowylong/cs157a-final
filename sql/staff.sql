5277407eb48d1f77ed45fa8db54ded66b43adba8
jowylong <89226403+jowylong@users.noreply.github.com>
2023-05-14 21:33:09 -0700

    Update staff.sql

--- sql/staff.sql
+++ sql/staff.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 14, 2023 at 05:16 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cs157a_final`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `staff`
---
-
 CREATE TABLE `staff` (
   `staff_id` int(5) NOT NULL,
   `staff_first_name` varchar(64) NOT NULL,
@@ -34,26 +5,8 @@ CREATE TABLE `staff` (
   `staff_phone` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `staff`
---
 
-INSERT INTO `staff` (`staff_id`, `staff_first_name`, `staff_last_name`, `staff_phone`) VALUES
-(11111, 'Jowyl', 'Ong', '408-555-5555'),
-(22222, 'Kaiming', 'Meng', '408-123-4567'),
-(33333, 'Nirupama', 'Tippabhatla', '408-987-6543');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `staff`
---
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`staff_id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
