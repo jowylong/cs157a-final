0a4e54df2418d617feea5b37395bc3dee0c61b5d
jowylong <89226403+jowylong@users.noreply.github.com>
2023-05-14 21:31:37 -0700

    Update game.sql

--- sql/game.sql
+++ sql/game.sql
@@ -1,49 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 14, 2023 at 05:18 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cs157a_final`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `game`
---
-
 CREATE TABLE `game` (
   `game_id` int(5) NOT NULL,
   `game_name` varchar(64) NOT NULL,
   `game_equipment_type` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `game`
---
 ALTER TABLE `game`
   ADD PRIMARY KEY (`game_id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
