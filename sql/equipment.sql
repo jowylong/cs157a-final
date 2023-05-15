5675fd9f4f763215d7640b183113043410eba2f8
jowylong <89226403+jowylong@users.noreply.github.com>
2023-05-14 21:31:16 -0700

    Update equipment.sql

--- sql/equipment.sql
+++ sql/equipment.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 14, 2023 at 05:18 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cs157a_final`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `equipment`
---
-
 CREATE TABLE `equipment` (
   `equipment_id` int(5) NOT NULL,
   `equipment_type` varchar(64) NOT NULL,
@@ -35,17 +6,8 @@ CREATE TABLE `equipment` (
   `equipment_seat_location` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `equipment`
---
 ALTER TABLE `equipment`
   ADD PRIMARY KEY (`equipment_id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
